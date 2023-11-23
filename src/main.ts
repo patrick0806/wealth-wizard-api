0c6079153ebe2068d4d92e6fee49d61315d7c26c
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-23 13:18:46 -0300

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfig } from './config/SwaggerConfig';
+import { API_BASE_PATH } from '@shared/constants/apiBasePath';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const swaggerConfig = new SwaggerConfig();
+
+  swaggerConfig.setupSwagger(`${API_BASE_PATH}/docs`, app);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
