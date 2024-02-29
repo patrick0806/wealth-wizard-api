b727bd817d08c62c49caebd4ad50d61d65496494
Patrick da Silva Nicezi <patrickk0806@gmail.com>
2024-02-29 04:06:23 -0300

    feat: create domain first classes

---
+++ src/shared/entities/expense.ts
@@ -0,0 +1,8 @@
+export class Expense {
+  id: string;
+  description: string;
+  value: number;
+  date: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/shared/entities/income.ts
@@ -0,0 +1,8 @@
+export class Income {
+  id: string;
+  description: string;
+  value: number;
+  date: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
